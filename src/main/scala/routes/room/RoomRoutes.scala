cd09e14afb5520f7c1dab25e82654c9c012067aa
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-29 01:27:34 +0600

    added current-user-rooms route

--- src/main/scala/routes/room/RoomRoutes.scala
+++ src/main/scala/routes/room/RoomRoutes.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.util.Timeout
 import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore
 import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore.GetRoomsForExploreRequest
-import kz.mounty.fm.domain.requests.{GetRoomAndRoomTracksRequestBody, GetRoomsForExploreRequestBody}
+import kz.mounty.fm.domain.requests.{GetCurrentUserRoomsRequestBody, GetRoomAndRoomTracksRequestBody, GetRoomsForExploreRequestBody}
 import org.json4s.jackson.Serialization.write
 import routes.RouteCompletion
 
@@ -35,6 +35,18 @@ trait RoomRoutes extends RouteCompletion{
 
         completeRequest(publisher, write(body), RoomCore.GetRoomsAndRoomTracksRequest.routingKey, exchange,  ctx)
 
+      }
+    }
+  } ~ pathPrefix("current-user-rooms") {
+    get {
+      parameters(
+        "limit".as[Int].optional,
+        "offset".as[Int].optional,
+        "tokenKey".as[String]
+      ) { (limit, offset, tokenKey) =>
+        ctx =>
+          val body = write(GetCurrentUserRoomsRequestBody(limit, offset, tokenKey))
+          completeRequest(publisher, write(body), RoomCore.GetCurrentUserRoomsRequest.routingKey, exchange, ctx)
       }
     }
   }

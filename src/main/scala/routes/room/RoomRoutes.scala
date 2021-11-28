09983bea2971dde3c00c5a25bf8c0addeff7ec0f
DSansyzbayev <sansyzbayevdaniyar3@gmail.com>
2021-11-29 00:15:55 +0600

    room player

--- src/main/scala/routes/room/RoomRoutes.scala
+++ src/main/scala/routes/room/RoomRoutes.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.util.Timeout
 import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore
 import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore.GetRoomsForExploreRequest
-import kz.mounty.fm.domain.requests.GetRoomsForExploreRequestBody
+import kz.mounty.fm.domain.requests.{GetRoomAndRoomTracksRequestBody, GetRoomsForExploreRequestBody}
 import org.json4s.jackson.Serialization.write
 import routes.RouteCompletion
 
@@ -21,6 +21,20 @@ trait RoomRoutes extends RouteCompletion{
       parameters("size".as[Int]) { size => ctx =>
         val body = write(GetRoomsForExploreRequestBody(size))
         completeRequest(publisher, write(body), RoomCore.GetRoomsForExploreRequest.routingKey, exchange,  ctx)
+      }
+    }
+  } ~ pathPrefix("room-and-track") {
+    get {
+      parameters(
+        "roomId".as[String],
+        "limit".as[Int].optional,
+        "offset".as[Int].optional,
+        "tokenKey".as[String]
+      ) { (roomId, limit, offset, tokenKey) => ctx =>
+        val body = write(GetRoomAndRoomTracksRequestBody(roomId, limit, offset, tokenKey))
+
+        completeRequest(publisher, write(body), RoomCore.GetRoomsAndRoomTracksRequest.routingKey, exchange,  ctx)
+
       }
     }
   }

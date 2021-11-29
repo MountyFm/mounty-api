7b7796e66e6118a9fa56205d6a3165116e44da74
dmoldabayev <dmoldabayev.99@gmail.com>
2021-11-29 10:23:35 +0600

    added getting room by inviteCode

--- src/main/scala/routes/room/RoomRoutes.scala
+++ src/main/scala/routes/room/RoomRoutes.scala
@@ -4,23 +4,24 @@ import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.server.Directives._
 import akka.util.Timeout
 import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore
-import kz.mounty.fm.amqp.messages.MountyMessages.RoomCore.GetRoomsForExploreRequest
-import kz.mounty.fm.domain.requests.{GetCurrentUserRoomsRequestBody, GetRoomAndRoomTracksRequestBody, GetRoomsForExploreRequestBody}
+import kz.mounty.fm.domain.requests._
 import org.json4s.jackson.Serialization.write
 import routes.RouteCompletion
 
 import scala.concurrent.ExecutionContext
 
-trait RoomRoutes extends RouteCompletion{
+trait RoomRoutes extends RouteCompletion {
   val exchange = "X:mounty-api-in"
+
   def roomRoutes(implicit ex: ExecutionContext,
                  publisher: ActorRef,
                  system: ActorSystem,
                  timeout: Timeout) = pathPrefix("explore") {
     get {
-      parameters("size".as[Int]) { size => ctx =>
-        val body = write(GetRoomsForExploreRequestBody(size))
-        completeRequest(publisher, write(body), RoomCore.GetRoomsForExploreRequest.routingKey, exchange,  ctx)
+      parameters("size".as[Int]) { size =>
+        ctx =>
+          val bodyJson = write(GetRoomsForExploreRequestBody(size))
+          completeRequest(publisher, bodyJson, RoomCore.GetRoomsForExploreRequest.routingKey, exchange, ctx)
       }
     }
   } ~ pathPrefix("room-and-track") {
@@ -30,10 +31,11 @@ trait RoomRoutes extends RouteCompletion{
         "limit".as[Int].optional,
         "offset".as[Int].optional,
         "tokenKey".as[String]
-      ) { (roomId, limit, offset, tokenKey) => ctx =>
-        val body = write(GetRoomAndRoomTracksRequestBody(roomId, limit, offset, tokenKey))
+      ) { (roomId, limit, offset, tokenKey) =>
+        ctx =>
+          val bodyJson = write(GetRoomAndRoomTracksRequestBody(roomId, limit, offset, tokenKey))
 
-        completeRequest(publisher, write(body), RoomCore.GetRoomsAndRoomTracksRequest.routingKey, exchange,  ctx)
+          completeRequest(publisher, bodyJson, RoomCore.GetRoomsAndRoomTracksRequest.routingKey, exchange, ctx)
 
       }
     }
@@ -45,8 +47,18 @@ trait RoomRoutes extends RouteCompletion{
         "tokenKey".as[String]
       ) { (limit, offset, tokenKey) =>
         ctx =>
-          val body = write(GetCurrentUserRoomsRequestBody(limit, offset, tokenKey))
-          completeRequest(publisher, write(body), RoomCore.GetCurrentUserRoomsRequest.routingKey, exchange, ctx)
+          val bodyJson = write(GetCurrentUserRoomsRequestBody(limit, offset, tokenKey))
+          completeRequest(publisher, bodyJson, RoomCore.GetCurrentUserRoomsRequest.routingKey, exchange, ctx)
+      }
+    }
+  } ~ pathPrefix("current-user-rooms") {
+    get {
+      parameters(
+        "inviteCode".as[String]
+      ) { inviteCode =>
+        ctx =>
+          val bodyJson = write(GetRoomByInviteCodeRequestBody(inviteCode))
+          completeRequest(publisher, bodyJson, RoomCore.GetRoomByInviteCodeRequest.routingKey, exchange, ctx)
       }
     }
   }
